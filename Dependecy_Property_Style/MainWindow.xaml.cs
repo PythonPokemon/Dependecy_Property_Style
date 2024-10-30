084d2c30e223dda979d9e187e9d9866a0cb1f408
jakob.derzapf <jakob.derzapf@WASNEZOW>
2024-10-30 10:51:06 +0100

    Projektdateien hinzufügen.

---
+++ Dependecy_Property_Style/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/*
+Dependency ==   Abhängigkeit
+Properties ==   Eigenschaften
+Controls   ==   Steuerung
+
+
+ */
+
+namespace Dependecy_Property_Style
+{
+    public partial class MainWindow : Window
+    {
+
+        // Eigenen Property anlegen | Eigenschaft
+        public int MyProperty
+        {
+            get { return (int)GetValue(myDependencyProperty); }
+            set { SetValue(myDependencyProperty, value); }
+        }
+
+        public static readonly DependencyProperty myDependencyProperty =
+            DependencyProperty.Register("MyProperty", typeof(int), typeof(MainWindow), new PropertyMetadata(0));
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
